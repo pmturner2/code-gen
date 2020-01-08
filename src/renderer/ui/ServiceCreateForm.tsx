18dd080140c8fcec7f690943212119b4813775be
Peter Turner <pturner@zynga.com>
2020-01-08 12:58:27 -0500

    Use FormAddMulti for ServiceCreation

--- src/renderer/ui/ServiceCreateForm.tsx
+++ src/renderer/ui/ServiceCreateForm.tsx
@@ -1,10 +1,6 @@
-import { Button, Divider, IconButton } from '@material-ui/core';
-import AddCircle from '@material-ui/icons/AddCircle';
-import RemoveCircle from '@material-ui/icons/RemoveCircle';
 import React from 'react';
 import { InjectableCategory } from '../Constants';
 import { generateService } from '../generator/ServiceGenerator';
-import { cloneMap } from '../generator/Utils';
 import {
   HttpRequestVerb,
   INewInjectable,
@@ -13,7 +9,7 @@ import {
   RetryPolicy,
   ZsrRequestService,
 } from '../Types';
-import { FormSection } from './common/FormSection';
+import { FormAddMultipleSection } from './common/FormAddMultipleSection';
 import { InjectableCreateForm } from './InjectableCreateForm';
 import { ZsrApiForm } from './ZsrApiForm';
 
@@ -22,37 +18,14 @@ interface IProps {
 }
 
 export const ServiceCreateForm: React.FunctionComponent<IProps> = props => {
-  const [zsrApis, setZsrApis] = React.useState(new Map<string, IZsrRequest>());
-  const [nextKey, setNextKey] = React.useState(0);
+  const [zsrRequests, setZsrRequests] = React.useState<IZsrRequest[]>([]);
 
-  const handleAddZsrApi = () => {
-    const key = nextKey.toString();
-    zsrApis.set(key, {
-      method: '',
-      retryPolicy: RetryPolicy.Exponential,
-      service: ZsrRequestService.Gwf,
-      verb: HttpRequestVerb.Get,
-    });
-    setZsrApis(cloneMap(zsrApis));
-    setNextKey(nextKey + 1);
+  const handleZsrRequestsChange = (requests: IZsrRequest[]) => {
+    setZsrRequests([...requests]);
   };
 
-  function createZsrApiRemoveHandler(key: string) {
-    return () => {
-      zsrApis.delete(key);
-      setZsrApis(cloneMap(zsrApis));
-    };
-  }
-
-  function createZsrApiFormChangeHandler(key: string) {
-    return (requestData: IZsrRequest) => {
-      zsrApis.set(key, requestData);
-      setZsrApis(cloneMap(zsrApis));
-    };
-  }
-
   const forceAddDependencies = () => {
-    if (zsrApis.size > 0) {
+    if (zsrRequests.length > 0) {
       return ['ServiceTypes.Logging', 'ServiceTypes.ZSR'];
     }
     return [];
@@ -67,10 +40,10 @@ export const ServiceCreateForm: React.FunctionComponent<IProps> = props => {
       {
         ...request,
         apiFilename:
-          zsrApis.size !== 0
+          zsrRequests.length !== 0
             ? `${request.name.substr(0, request.name.lastIndexOf('Service'))}Api`
             : undefined,
-        zsrRequests: Array.from(zsrApis.keys()).map(key => zsrApis.get(key)),
+        zsrRequests,
       },
       onSubmissionProgress,
     );
@@ -83,34 +56,18 @@ export const ServiceCreateForm: React.FunctionComponent<IProps> = props => {
       submit={submit}
       forceAddDependencies={forceAddDependencies}
     >
-      <FormSection title="Zynga Api Calls">
-        <Button
-          size={'large'}
-          style={{ alignSelf: 'flex-start' }}
-          fullWidth={false}
-          onClick={handleAddZsrApi}
-          startIcon={<AddCircle color="primary" />}
-        >
-          Add Api Call
-        </Button>
-        {Array.from(zsrApis.keys()).map(key => {
-          return (
-            <div key={key}>
-              <Divider />
-              <FormSection
-                startIcon={
-                  <IconButton onClick={createZsrApiRemoveHandler(key)}>
-                    <RemoveCircle color="error" />
-                  </IconButton>
-                }
-                title="Api Call"
-              >
-                <ZsrApiForm onChange={createZsrApiFormChangeHandler(key)} />
-              </FormSection>
-            </div>
-          );
-        })}
-      </FormSection>
+      <FormAddMultipleSection
+        title="Zynga Api Calls"
+        elementName="ApiCall"
+        onChange={handleZsrRequestsChange}
+        elementCreateForm={ZsrApiForm}
+        defaultElement={{
+          method: '',
+          retryPolicy: RetryPolicy.Exponential,
+          service: ZsrRequestService.Gwf,
+          verb: HttpRequestVerb.Get,
+        }}
+      />
     </InjectableCreateForm>
   );
 };
